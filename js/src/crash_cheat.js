c403036592bd1f79649efc7269915030967948f6
PYHerman <731207730@qq.com>
2019-09-12 14:47:50 +0800

    Site updated: 2019-09-12 14:47:43

--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -1,13 +1,14 @@
 var OriginTitle = document.title;
  var titleTime;
+
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
-         $('[rel="icon"]').attr('href', "/img/TEP.ico");
+         $('[rel="icon"]').attr('href', "/images/TEP.png");
          document.title = '→ ❤坤坤❤热舞中❤ ←';
          clearTimeout(titleTime);
      }
      else {
-         $('[rel="icon"]').attr('href', "/favicon.ico");
+         $('[rel="icon"]').attr('href', "/images/favicon.png");
          document.title = '(｡･ω･｡)嘻嘻,上当了吧 ' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
